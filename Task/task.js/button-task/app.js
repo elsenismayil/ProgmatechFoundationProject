923115f0a1870630c7abe112b711183cde55651f
Elsen Ismayilov <elsenismayilov0057@gmail.com>
2021-12-17 02:34:15 +0400

    button task

---
+++ Task/task.js/button-task/app.js
@@ -0,0 +1,19 @@
+let body = document.querySelector('body')
+function changeBg(){
+    body.style.background = "red"
+}
+
+let head = document.querySelector('h1')
+function changeHeading(){
+    head.style.color = "blue"
+}
+
+let metn = document.querySelector('p')
+function changeP(){
+    metn.style.color = "green"
+}
+
+let sekil = document.querySelector('img')
+function image(){
+    sekil.style.image = "src" , "https://res.cloudinary.com/practicaldev/image/fetch/s--UBVTT6xB--/c_imagga_scale,f_auto,fl_progressive,h_900,q_auto,w_1600/https://dev-to-uploads.s3.amazonaws.com/i/g83rrbcpsylwwxn9p92q.jpg"
+}
